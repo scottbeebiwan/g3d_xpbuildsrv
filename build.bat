d52700e8fec2d3602991a7842e6e7861bdc3b891
ScottBeebiWan <scottbeebiwan@yahoo.com>
2019-05-25 04:11:35 -0700

    another build.bat fix

--- build.bat
+++ build.bat
@@ -1,3 +1,7 @@
-call "C:\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat" x86
-
+@echo off
+set ocd=%cd%
+cd "\Program Files\Microsoft Visual Studio 8\VC"
+call "\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat" x86
+cd %ocd%
+cd G3D-Fun
 msbuild
